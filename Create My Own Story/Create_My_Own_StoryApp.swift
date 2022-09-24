b2a5de84d4579695622bc6d904d5f3f190df9dac
Giorgio Giannotta <jorge_upa@msn.com>
2022-09-24 17:15:34 +0200

    Initial Commit

---
+++ Create My Own Story/Create_My_Own_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Create_My_Own_StoryApp.swift
+//  Create My Own Story
+//
+//  Created by Giorgio Giannotta on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Create_My_Own_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
